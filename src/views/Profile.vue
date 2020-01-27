da4cd6b85ecd63f19615beefafcf06ede905173c
KatieLuzina <39306742+KatieLuzina@users.noreply.github.com>
2020-01-27 08:11:52 +0500

    Add files via upload

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -9,21 +9,32 @@
     </div>
   </div>
   <div class = "profile_repos">
-    Список репозиториев
+    Список репозиториев 
     <table>
       <tr v-for="item in repos.data" :key="item.name">
         <td>{{item.name}}</td>
         <td>
-          Описание : {{item.description}}
-          Язык программирования : 
-          <div v-for="(value, name) in item.languages" :key="name">
+          <b>Описание :</b> {{item.description}}<br>
+          <b>Язык программирования :</b> {{item.language}}<br>
+          <!--<div v-for="(value, name) in item.languages" :key="name">
             {{name}}
-          </div>
-          Дата создания : {{item.created_at}}
-          Ссылка на клонирование репозитория: {{item.clone_url}}
+          </div>-->
+          <b>Дата создания :</b> {{item.created_at}}<br>
+          <b>Ссылка на клонирование репозитория:</b> <a :href="item.clone_url">{{item.clone_url}}</a><br>
         </td>
       </tr>
     </table>
+  </div>
+  <div class = "profile_following">
+    Список подписок 
+      <div v-for="item in following.info" :key="item.login">
+        <div class = "profile_following_main">
+          <img class ="profile_following_img" :src="item.avatar_url">
+          <div class = "profile_following_descr">
+            Логин : <a class="profile_login" :href="item.html_url">{{item.login}}</a>
+          </div>
+        </div>        
+      </div>
   </div>
 </div>
 
@@ -40,12 +51,16 @@ export default {
       created_at: null,
       repos :  {
         data : {
+          created_at : null,
           languages : []
         } 
+      },
+      following : {
+        info: []
       }
     }
   },
-  created() {
+  mounted() {
       axios.get('https://api.github.com/users/KatieLuzina')
       .then(response => {
         this.info= response.data        
@@ -53,16 +68,22 @@ export default {
       })
       axios.get('https://api.github.com/users/KatieLuzina/repos')
       .then(response => {
-        this.repos.data= response.data
-        for (let i=0; i<this.repos.data.length; i++) {          
+        this.repos.data= response.data        
+        for (let i=0; i<this.repos.data.length; i++) {                   
           axios.get('https://api.github.com/repos/KatieLuzina/'+ this.repos.data[i].name +'/languages')
           .then(response => {
             this.repos.data[i].languages=response.data
-          })          
+          })
+          this.repos.data[i].created_at=(moment(response.data[i].created_at).locale('ru')).format('LLL')     
         }
+      })
+      axios.get('https://api.github.com/users/KatieLuzina/following')
+      .then(response => {
+        this.following.info= response.data        
       })
   }
 }
+
 </script>
 
 <style>
@@ -91,5 +112,49 @@ export default {
 .profile_repos {
   font-size: 22px;
   font-weight: bold;
+
+}
+table {
+  border: 1px solid #ccc;
+  border-spacing: 3px;
+  border-collapse: collapse;
+  margin: 20px 100px;
+  margin: 0 auto;
+}
+td{
+  border: solid 1px #ccc;
+  text-align: left;
+  padding: 5px;
+  font-size: 16px;
+  font-weight: normal;
+}
+a{
+  color: #42b983;
+}
+
+.profile_following {
+  margin : 20px 50px;
+  font-size: 22px;
+  font-weight: bold;
+}
+
+.profile_following_main {
+  padding: 25px 250px;
+  display: flex; 
+}
+.profile_following_img {
+  width: 150px;
+  margin-right: 50px;
+}
+.profile_following_descr {
+  font-size: 16px;
+  font-weight: bold;
+  line-height: 30px;
+  padding-top: 20px;
+}
+.profile_following_login {
+  display: block;
+  margin-bottom: 40px;
+  color: #42b983;
 }
 </style>
--- src/views/Team.vue
+++ src/views/Team.vue
@@ -1,7 +1,77 @@
 <template>
-  <div class="team">
-    ghdfj
+  <div class="myTeam">
+    <div  class="column">
+    <h2>Моя команда</h2>
+    <div class="user_info" v-for="item in myteam" :key="item.login">     
+      <img class ="myTeam_img" :src="item.avatar_url">
+      <div class = "user_descr">     
+        {{item.login}}
+        <br>
+        <a :href="item.html_url">{{item.html_url}}</a>
+      </div>
+    </div>
+    </div>
+    <div  class="column">
+    <h2>Пользователи</h2>
+    <div class = "user_info" v-for="item in users" :key="item.login">      
+      <img class ="myTeam_img" :src="item.avatar_url">
+      <div class = "user_descr">
+        {{item.login}}
+        <br>
+        <a :href="item.html_url">{{item.html_url}}</a>   
+      </div>  
+    </div>
+    </div>
   </div>
 </template>
 
 
+<script>
+import axios from 'axios'
+export default {
+  name: 'Team',
+  data() {
+    return {
+      users : [],
+      myteam : []
+    }
+  },
+  mounted() {
+      axios.get('https://api.github.com/users?since=50000000')
+      .then(response => {
+        this.users = response.data
+        for (let i=0 ;i<5 ;i++ ) {
+          this.myteam.push(this.users[i])
+          this.users.shift()        
+      }
+
+      })
+
+
+  }
+}
+
+</script>
+
+<style>
+.myTeam {
+  display: flex;
+  justify-content: center;
+}
+.column {
+  display: flex;
+  flex-direction: column;
+  margin: 10px 50px;
+}
+.user_info {
+  display: flex;
+  margin: 30px;
+}
+.user_info img {
+  width: 150px;
+  
+}
+.user_descr {
+  margin: 30px;
+}
+</style>
